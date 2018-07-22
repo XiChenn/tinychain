e724660888968e4789dd1e91b064d62a98e5d6fa
Xi Chen <chenx19@miamioh.edu>
2018-07-22 14:30:51 -0400

    Add proof of work and dynamic adjust difficulty (#2)

---
+++ config.js
@@ -0,0 +1,4 @@
+const DIFFICULTY = 3; // Number of preceding zeros
+const EXPECTED_MINING_TIME = 3000; // Expected mining time milliseconds
+
+export { DIFFICULTY, EXPECTED_MINING_TIME };
--- test/block.test.js
+++ test/block.test.js
@@ -16,4 +16,16 @@ describe('Block', () => {
     it('sets the `hashPrev` to match the hash of the last block', () => {
        expect(block.hashPrev).toEqual(blockPrev.hash);
     });
+
+    it('generates a hash that matches the difficulty', () => {
+        expect(block.hash.substring(0, block.bits)).toEqual('0'.repeat(block.bits));
+    });
+
+    it('lowers the difficulty for slowly mined blocks', () => {
+        expect(Block.adjustDifficulty(block.time + 360000, block)).toEqual(block.bits - 1);
+    });
+
+    it('raises the difficulty for fast mined blocks', () => {
+        expect(Block.adjustDifficulty(block.time + 1, block)).toEqual(block.bits + 1);
+    });
 });
